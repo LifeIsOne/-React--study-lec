9f74ef6b99cb9943e03973949f53e9645c348547
LifeIsOne <didtmdgh258@gmail.com>
2024-07-04 12:01:49 +0900

    form 값으로 로그인

--- src/page/LoginComplete.js
+++ src/page/LoginComplete.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../store';
 
 function LoginComplete(props) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
+    function logoutProcess (){
+ 
+        // 1. store 상태변경
+        dispatch(logout());
+
+        // 2. 화면 이동
+        navigate("/loginComplete");
+    }
     const isLogin = useSelector((state) => state.isLogin);
 
     return (
         <div>
             <h1>Login 완료</h1>
             <h2>store isLogin : {isLogin.toString()}</h2>
+            <button onClick={logoutProcess}>로그아웃</button>
         </div>
     );
 }
